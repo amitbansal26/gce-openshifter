54d4cbecfcaf6aad2b342cbfd383a1f509abcbbe
Charles Moulliard <cmoulliard@apache.org>
2017-06-06 10:11:16 +0200

    Remove comments non required

--- create_project.sh
+++ create_project.sh
@@ -6,7 +6,8 @@ if [ "$#" -ne 2 ]; then
    exit
 fi
 
-ACCOUNT_ID=$(gcloud alpha billing accounts list | awk 'FNR == 2 {print $1}')
+# FNR value should be changed if you have several accounts"
+ACCOUNT_ID=$(gcloud alpha billing accounts list | awk 'FNR == 3 {print $1}')
 PROJECT_PREFIX=${1:-project}
 EMAIL=${2:-cmoulliard@redhat.com}
 REGION=${3:-europe-west1}
@@ -15,11 +16,6 @@ SERVICEACCOUNT="my-sa-1"
 
 PROJECT_ID=$(echo "${PROJECT_PREFIX}-${EMAIL}" | sed 's/@/-/g' | sed 's/\./-/g' | cut -c 1-30)
 
-# echo "Update gcloud client"
-# gcloud components update
-# gcloud components install alpha
-
-#PROJECT_ID=$(echo "${PROJECT_PREFIX}-${EMAIL}" | sed 's/@/x/g' | sed 's/\./x/g' | cut -c 1-30)
 echo "Creating project $PROJECT_ID ... "
 gcloud projects create $PROJECT_ID
 sleep 2
@@ -39,9 +35,6 @@ gcloud service-management enable compute-component.googleapis.com
 
 echo ">>> Change default location"
 gcloud compute project-info add-metadata --metadata google-compute-default-region=$REGION,google-compute-default-zone=$ZONE
-
-set -x
-trap read debug
 
 echo ">>> Add role owner to the user"
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="user:$EMAIL" --role="roles/owner"
