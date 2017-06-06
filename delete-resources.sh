eabe276ef9764619e241f8803004257b59eb0f48
Charles Moulliard <cmoulliard@apache.org>
2017-06-06 12:06:18 +0200

    Update doc and script to delete resources

--- delete-resources.sh
+++ delete-resources.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for i in {1..10}; do
+INSTANCES=${1:-10}
+
+for ((i=1; i<=INSTANCES; i++)); do
   VM_NAME="vm-$i"
   gcloud compute disks delete $VM_NAME-master-docker --quiet
   gcloud compute disks delete $VM_NAME-master-root --quiet
