cfc3c14833d879dfb3b89ac5962797d2c3a924e1
Charles Moulliard <cmoulliard@apache.org>
2017-06-07 09:38:55 +0200

    Switch position of a delete command

--- delete-resources.sh
+++ delete-resources.sh
@@ -6,10 +6,10 @@ for ((i=1; i<=INSTANCES; i++)); do
   VM_NAME="vm-$i"
   gcloud compute disks delete $VM_NAME-master-docker --quiet
   gcloud compute disks delete $VM_NAME-master-root --quiet
-  gcloud compute addresses delete $VM_NAME-master --quiet
   gcloud compute firewall-rules delete $VM_NAME-internal --quiet
   gcloud compute firewall-rules delete $VM_NAME-master --quiet
   gcloud compute firewall-rules delete $VM_NAME-all --quiet
   gcloud compute firewall-rules delete $VM_NAME-infra --quiet
+  gcloud compute addresses delete $VM_NAME-master --quiet
   gcloud compute networks delete $VM_NAME --quiet
 done
